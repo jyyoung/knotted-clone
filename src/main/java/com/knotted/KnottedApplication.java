6395443345d2d446732616840704c4f7c4ceea93
jungmin0917 <cloonds@gmail.com>
2023-05-05 16:18:33 +0900

    노티드 프로젝트 생성 후 첫 커밋

---
+++ src/main/java/com/knotted/KnottedApplication.java
@@ -0,0 +1,13 @@
+package com.knotted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnottedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnottedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knotted/KnottedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knotted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnottedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
