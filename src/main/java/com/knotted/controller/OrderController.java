25ab3be23b8c4eab14446f98bf8af4be7e28a9c8
jungmin0917 <cloonds@gmail.com>
2023-05-15 20:03:27 +0900

    사용자 페이지 개발 - 예약 매장선택 부분 구현 중

--- src/main/java/com/knotted/controller/OrderController.java
+++ src/main/java/com/knotted/controller/OrderController.java
@@ -19,9 +19,29 @@ public class OrderController {
 
     // 예약 - 매장선택 페이지로 이동
     @GetMapping(value = "/store-pick")
-    public String storePick(){
-        return "/order/storePick";
+    public String storePick(Model model){
+        String mode = "store";
+
+        model.addAttribute("mode", mode);
+        return "/order/orderSelect";
+    }
+    
+    // 예약 - 일자선택 페이지로 이동
+    @GetMapping(value = "/date-pick")
+    public String datePick(Model model){
+        String mode = "date";
+
+        model.addAttribute("mode", mode);
+        return "/order/orderSelect";
     }
 
+    // 예약 - 메뉴선택 페이지로 이동
+    @GetMapping(value = "/menu-pick")
+    public String menuPick(Model model){
+        String mode = "menu";
+
+        model.addAttribute("mode", mode);
+        return "/order/orderSelect";
+    }
 
 }
