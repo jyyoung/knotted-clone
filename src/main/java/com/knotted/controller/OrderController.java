73366257c9f6d8ea0c9f22abe15b5636f8300e35
jungmin0917 <cloonds@gmail.com>
2023-05-15 18:11:46 +0900

    사용자 페이지 개발 - 예약 메인 (날짜선택예약, 팝업 등) 구현 완료.

--- src/main/java/com/knotted/controller/OrderController.java
+++ src/main/java/com/knotted/controller/OrderController.java
@@ -11,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class OrderController {
 
+    // 주문(예약) 메인으로 이동
     @GetMapping(value = {"", "/"})
     public String main(Model model){
         return "/order/index";
     }
+
+    // 예약 - 매장선택 페이지로 이동
+    @GetMapping(value = "/store-pick")
+    public String storePick(){
+        return "/order/storePick";
+    }
+
+
 }
