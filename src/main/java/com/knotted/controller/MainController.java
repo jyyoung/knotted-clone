75257bad80c7af28be6dedf6775533998f9ec880
jungmin0917 <jungmin09172@naver.com>
2023-05-23 12:33:29 +0900

    사용자 페이지 개발 - 메인 페이지 부분 베스트 메뉴 제외하고 구현 완료.

--- src/main/java/com/knotted/controller/MainController.java
+++ src/main/java/com/knotted/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.knotted.controller;
 
+import com.knotted.dto.BoardDTO;
+import com.knotted.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final BoardService boardService;
+
+    // 메인 페이지로 이동. 베스트 메뉴, 이벤트도 같이 넘김
     @GetMapping(value = "/")
     public String main(Model model){
+        List<BoardDTO> boardDTOList = boardService.getCategoryBoards("event");
+
+        model.addAttribute("boardDTOList", boardDTOList);
+
         return "/index";
     }
 }
